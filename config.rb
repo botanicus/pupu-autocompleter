d48d57592b62ce7f77fcb7b71d0ddcc4c899933a
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2008-12-02 19:42:17 +0100

    Added better spinner

--- config.rb
+++ config.rb
@@ -4,6 +4,9 @@ dependencies :mootools
 # javascripts for loading
 javascripts "observer", "autocompleter"
 
+# stylesheet
+stylesheet "autocompleter"
+
 parameter :type, :optional => ["local", "request"] do |type|
   javascript "autocompleter.#{type}"
 end
